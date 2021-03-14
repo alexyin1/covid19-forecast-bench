829f00ee823fa542395e4345767d04a8e862bbd4
Alex Yin <alexyin@usc.edu>
2021-03-13 19:30:45 -0800

    fixed various data bugs

--- gen_json.py
+++ gen_json.py
@@ -3,19 +3,23 @@ import os
 import json
 import collections
 from datetime import datetime, date, timedelta
+from operator import add
+import sys
 
+INGORED_FF_COLS = ['State', 'County'] # counties to be ignored from formatted forecasts
 JSON_DIR = 'json-forecasts'
 CASE_DIR = 'formatted-forecasts'
 FORECAST_TYPES = ['county-case', 'state-case', 'state-death']
 DAY_ZERO = datetime(2020, 1, 22) # 2020-1-23 is considered startdate of pand. in U.S.
 
-#turn ndays to actual dates, round to previous sunday
-def to_date(ndays):
+#turn ndays to actual dates
+def to_date(ndays, round_to_sunday=False):
     delta = timedelta(days=int(ndays))
     date_obj = DAY_ZERO + delta
-    one_day = timedelta(days=1)
-    while date_obj.weekday() < 6:
-        date_obj -= one_day
+    if round_to_sunday:
+        one_day = timedelta(days=1)
+        while date_obj.weekday() < 6:
+            date_obj -= one_day
     return date_obj.strftime("%m-%d-%Y")
 
 
@@ -56,18 +60,41 @@ def read_forecast(forecast_path, model_name: str)-> dict:
 
 
 def read_single_csv(path: str) -> dict:
-    df = pd.read_csv(path, index_col=0)
+    cols = list(pd.read_csv(path, index_col=0, nrows=1))
+    # skip the smallest ndays in formatted forecast, because that is a sum value
+    min_key = sys.maxsize
+    for key in cols:
+        min_key = int(key) if key.isnumeric() and int(key) < min_key else min_key 
+    wanted_cols = [i for i in cols if i not in INGORED_FF_COLS and i != min_key]
+    # only read wanted cols
+    df= pd.read_csv(path, index_col=0, usecols=wanted_cols)
     df.fillna(value='null', inplace=True)
     cases = {}
     for i, row in enumerate(df.iterrows()):
         case_data = row[1]
         for label in case_data.index:
-            if label != 'State' and label != 'County':
-                if label in cases:
-                    cases[label].append(case_data[label])
-                else:
-                    cases[label] = []
+            if label not in cases:
+                cases[label] = []
+            case = case_data[label]
+            if not isinstance(case, str):
+                case = float(case)
+            cases[label].append(case)
     return cases
+
+# round all date entries into nearest sunday
+def condense_dates(data: dict) -> dict:
+    temp = {}
+    for ndays,vals in data.items():
+        nearest_sunday = to_date(ndays)
+        if nearest_sunday not in temp:
+            temp[nearest_sunday] = vals
+        else:
+            for i in range(len(vals)):
+                if temp[nearest_sunday][i] == 'null':
+                    temp[nearest_sunday][i] = vals[i]
+                elif vals[i] != 'null':
+                    temp[nearest_sunday][i] = str(int(temp[nearest_sunday][i])+int(vals[i])) 
+    return temp
 
 
 def main():
